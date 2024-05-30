98af683ee70666d4beef1a39dd87bcf29a35b120
Psonbre <adam.maps.2003@gmail.com>
2024-05-29 22:17:24 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`); 
+});
+
